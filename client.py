45862fb115919bd9228369ba9a26176e43551ccd
william-zhang-ml <william.zhang.ml@gmail.com>
2024-12-28 00:29:37 -0500

    update button control flow

--- client.py
+++ client.py
@@ -15,7 +15,23 @@ class UserInterface(BoxLayout):
     _button = ObjectProperty(None)
 
     def _handle_press(self) -> None:
-        return
+        """Handle button press, either submit an image or reset. """
+
+        if self._camera.play:
+            self._button.disabled = True             # disable user input
+            self._camera.play = False                # pause camera
+            pass                                     # submit image and wait
+            self._label.text = 'HOTDOG'              # show result
+            self._label.color = (0, 1, 0, 1)         # show result
+            self._button.text = 'Reset!'             # inform user
+            self._button.disabled = False            # enable user input
+        else:
+            self._button.disabled = True             # disable user input
+            self._camera.play = True                 # unpause camera
+            self._label.text = 'Take a picture!'     # reset prompt
+            self._label.color = (1, 1, 1, 1)         # reset prompt
+            self._button.text = 'Is this a hotdog?'  # inform user
+            self._button.disabled = False            # enable user input
 
     def _handle_response(
         self,
